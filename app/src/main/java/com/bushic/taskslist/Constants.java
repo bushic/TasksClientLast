b8feee7a06304bcb91f069ff7a59cab8685a260b
bushic <bushic@mail.ru>
2017-01-10 13:42:26 +0500

    First

--- app/src/main/java/com/bushic/taskslist/Constants.java
+++ app/src/main/java/com/bushic/taskslist/Constants.java
@@ -3,6 +3,10 @@ package com.bushic.taskslist;
 public class Constants {
     public static class URL{
         private static final String HOST = "http://192.168.1.4:8080/";
-        public static final String GET_USERBYID = HOST + "users/";
+        //public static final String GET_USERBYID = HOST + "users/";
+        public static final String GET_USERBYLOGIN = HOST + "users/";
+        public static final String GET_LISTBYID= HOST + "lists/";
+        public static final String GET_TASKSBYLISTID = HOST + "tasks/";
+        public static final String GET_PERMISSONSBYUSERID = HOST + "permissions/";
     }
 }
--- app/src/main/java/com/bushic/taskslist/RegistrationActivity.java
+++ app/src/main/java/com/bushic/taskslist/RegistrationActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -19,6 +20,8 @@ import java.util.List;
  * Created by Дмитрий on 19.07.2016.
  */
 public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public RegistrationActivity registrationActivity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +37,47 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        //startActivity(intent);
-        new TasksListUser().execute();
+
+        EditText editLogin = (EditText) findViewById(R.id.editLogin);
+        EditText editPassword = (EditText) findViewById(R.id.editPassword);
+        String[] params = {editLogin.getText().toString(),editPassword.getText().toString()};
+
+        new TasksListUser().execute(params);
+
+
     }
 
-    public class TasksListUser extends AsyncTask<Void,Void,User>{
+    public class TasksListUser extends AsyncTask<String,Void,User>{
 
         @Override
-        protected User doInBackground(Void... params) {
+        protected User doInBackground(String... params) {
             RestTemplate template = new RestTemplate();
             template.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+            User user = template.getForObject(Constants.URL.GET_USERBYLOGIN+params[0], User.class);
 
-            return template.getForObject(Constants.URL.GET_USERBYID + "1",User.class);
+            if (user != null)
+                return null;
+            else {
+                User newUser = new User();
+
+                newUser.setLogin(params[0]);
+                newUser.setPassword(params[1]);
+                newUser.setRoot(false);
+
+                return template.postForObject(Constants.URL.GET_USERBYLOGIN,newUser,User.class);
+            }
         }
 
         @Override
         protected void onPostExecute(User user) {
-            List<User> list = new ArrayList<>();
-            list.add(user);
-
             TextView text = (TextView)findViewById(R.id.textViewReg);
-            text.setText(user.getLogin());
+
+            if (user == null)
+                text.setText("Пользователь с таким логином уже зарегистрирован");
+            else{
+                Intent intent = new Intent(registrationActivity, LoginActivity.class);
+                startActivity(intent);
+            }
         }
     }
 }
